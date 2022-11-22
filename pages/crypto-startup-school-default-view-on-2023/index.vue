6d04f9add7c08ea413b16785e32ee4fc528cc347
olehyev138 <oleg.ye@yahoo.com>
2022-11-22 20:30:19 +0600

    convert crypto-start-up-school-default-view-on-2023 page

---
+++ pages/crypto-startup-school-default-view-on-2023/index.vue
@@ -0,0 +1,278 @@
+<template>
+  <div id="wrapper">
+    <header class="hero hero--sm hero--startup">
+      <div class="container">
+        <picture class="bg">
+          <source
+            media="(min-width: 768px)"
+            srcset="@/assets/images/bg-banner-startup.svg"
+          />
+          <source srcset="@/assets/images/bg-banner-startup.svg" />
+          <img src="@/assets/images/bg-banner-startup.svg" alt="" />
+        </picture>
+        <div class="highlight-display hld--pacific-custom">
+          <h1><span>crypto</span><span>startup</span><span>school</span></h1>
+        </div>
+      </div>
+    </header>
+    <div class="text-block text-block--startup">
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-6">
+            <h4>
+              Build your product alongside fellow founders and experienced
+              entrepreneurs.
+            </h4>
+          </div>
+          <div class="col-sm-6">
+            <div class="desc">
+              <p>
+                Work with industry experts as you navigate the startup terrain.
+                Leverage the a16z network to help take your product mainstream.
+                <a href="#">Crypto Startup School (CSS23)</a> will take place in
+                Los Angeles, CA from March 6 - May 26, 2023
+              </p>
+              <p>Sign up to stay in the loop on CSS news and updates:</p>
+              <form class="subscribe-form" action="#">
+                <div class="form-group">
+                  <div class="input">
+                    <input type="email" placeholder="Enter email address" />
+                    <button type="submit" value="subscribe">subscribe</button>
+                  </div>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="section-divider">
+      <div class="container">
+        <span class="block-title">Course videos</span>
+      </div>
+    </div>
+    <section class="course">
+      <div class="container">
+        <div class="text">
+          <h6>
+            Course videos will be be posted after the program concludes in May
+            2023.
+          </h6>
+          <p>View videos for <a href="#">2020</a> here.</p>
+        </div>
+      </div>
+    </section>
+    <div class="section-divider bg-gray">
+      <div class="container">
+        <span class="block-title">Curriculum </span>
+      </div>
+    </div>
+    <section class="curriculums bg-gray">
+      <div class="container">
+        <div class="intro">
+          <h6>
+            We plan to cover the following topics through lectures, mentor
+            office hours, and founder talks:
+          </h6>
+        </div>
+        <ul class="curriculums-list">
+          <li>
+            <span class="count">01 /</span>
+            <h6><a href="#">web3 big picture</a></h6>
+          </li>
+          <li>
+            <span class="count">02 /</span>
+            <h6><a href="#">protocol development</a></h6>
+          </li>
+          <li>
+            <span class="count">03 /</span>
+            <h6><a href="#">cryptographic and mechanism design tools</a></h6>
+          </li>
+          <li>
+            <span class="count">04 /</span>
+            <h6><a href="#">web3 go-to-market</a></h6>
+          </li>
+          <li>
+            <span class="count">05 /</span>
+            <h6><a href="#">state of crypto regulation</a></h6>
+          </li>
+          <li>
+            <span class="count">06 /</span>
+            <h6><a href="#">legal design & the role of decentralization</a></h6>
+          </li>
+          <li>
+            <span class="count">07 /</span>
+            <h6><a href="#">company building in web3</a></h6>
+          </li>
+          <li>
+            <span class="count">08 /</span>
+            <h6>
+              <a href="#">community building & decentralized organizations</a>
+            </h6>
+          </li>
+          <li>
+            <span class="count">09 /</span>
+            <h6><a href="#">smart contract & custody security</a></h6>
+          </li>
+          <li>
+            <span class="count">10 /</span>
+            <h6><a href="#">branding & design</a></h6>
+          </li>
+          <li>
+            <span class="count">11 /</span>
+            <h6><a href="#">fundraising best practices</a></h6>
+          </li>
+        </ul>
+        <div class="curriculums-footer">
+          <p>
+            *The final curriculum and instructor list are coming soon. In the
+            meantime, we will be sharing program updates on our social channels.
+          </p>
+        </div>
+      </div>
+    </section>
+    <div class="section-divider">
+      <div class="container">
+        <span class="block-title">Instructors & Advisors</span>
+      </div>
+    </div>
+    <section class="course">
+      <div class="container">
+        <div class="text">
+          <h6>
+            The full list of 2023 CSS instructors and advisors is coming soon.
+          </h6>
+          <p>
+            In the meantime, we will be sharing program updates on our social
+            channels. View 2020 instructors and advisors.
+          </p>
+        </div>
+      </div>
+    </section>
+    <div class="section-divider">
+      <div class="container">
+        <span class="block-title">Course reader</span>
+      </div>
+    </div>
+    <section class="course">
+      <div class="container">
+        <div class="text">
+          <h6>
+            The course reader for 2023 will be posted after the program
+            concludes in May 2023.
+          </h6>
+          <p>View <a href="#">2020</a> course reader.</p>
+        </div>
+      </div>
+    </section>
+    <div class="section-divider">
+      <div class="container">
+        <span class="block-title">Alumni</span>
+      </div>
+    </div>
+    <section class="course">
+      <div class="container">
+        <div class="text">
+          <h6>
+            Alumni for 2023 will be posted after the program concludes in May
+            2023.
+          </h6>
+          <p>View <a href="#">2020</a> alumni.</p>
+        </div>
+      </div>
+    </section>
+    <div class="section-divider">
+      <div class="container">
+        <span class="block-title">FAQ</span>
+      </div>
+    </div>
+    <section class="faq">
+      <div class="container">
+        <ul class="faq-list">
+          <li><a href="#" class="opener">What is Crypto Startup School?</a></li>
+          <li>
+            <a href="#" class="opener">Does the program include funding?</a>
+          </li>
+          <li><a href="#" class="opener">What are the other benefits?</a></li>
+          <li>
+            <a href="#" class="opener"
+              >When and where does the program take place? Can I participate
+              remotely?</a
+            >
+          </li>
+          <li>
+            <a href="#" class="opener">Who should apply for the program? </a>
+          </li>
+          <li>
+            <a href="#" class="opener">How many companies will be accepted? </a>
+          </li>
+          <li>
+            <a href="#" class="opener">How many companies will be accepted? </a>
+          </li>
+          <li>
+            <a href="#" class="opener"
+              >Are teams required to have a live product or company before
+              applying?</a
+            >
+          </li>
+          <li>
+            <a href="#" class="opener"
+              >Are teams required to have a live product or company before
+              applying?</a
+            >
+          </li>
+          <li>
+            <a href="#" class="opener"
+              >Can companies participate if they have already raised funding?</a
+            >
+          </li>
+          <li>
+            <a href="#" class="opener">What is the weekly time commitment?</a>
+          </li>
+          <li>
+            <a href="#" class="opener"
+              >What is the application and review process?</a
+            >
+          </li>
+          <li>
+            <a href="#" class="opener"
+              >What happens at the end of the program?</a
+            >
+          </li>
+          <li>
+            <a href="#" class="opener"
+              >Will you provide dedicated space for the teams to work out of in
+              Los Angeles, CA?</a
+            >
+          </li>
+          <!-- add [active] class on opener to show active state  -->
+          <li>
+            <a href="#" class="opener"
+              >Do participants have to be US citizens?</a
+            >
+          </li>
+        </ul>
+        <div class="faq-footer">
+          <p>
+            Questions? Please email us at
+            <a href="mailto:crypto-startup-school@a16z.com"
+              >crypto-startup-school@a16z.com</a
+            >
+          </p>
+        </div>
+      </div>
+    </section>
+    <Footer></Footer>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "cryptoStartupSchoolDefaultViewOn2023Page",
+  head() {
+    return {
+      title: "Crypto Startup School Default ViewOn 2023",
+    };
+  },
+};
+</script>
