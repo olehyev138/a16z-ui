3f188a9ae7b8497860ba6514ae3b4bdc8bef3c42
olehyev138 <oleg.ye@yahoo.com>
2022-11-23 23:34:42 +0600

    make instructors & Advisors components

---
+++ components/Advisors.vue
@@ -0,0 +1,230 @@
+<template>
+  <div class="instructors-section">
+    <div class="container">
+      <h6>Advisors</h6>
+      <ul class="instructors-list">
+        <li>
+          <a href="#" class="card-instructors">
+            <div class="avatar">
+              <img
+                src="@/assets/images/avatar-arianna.png"
+                alt="Arianna Simpson"
+              />
+            </div>
+            <div class="infos">
+              <span class="name">Arianna Simpson</span>
+              <span class="designation">General Partner, a16z crypto</span>
+            </div>
+          </a>
+        </li>
+        <li>
+          <a href="#" class="card-instructors">
+            <div class="avatar">
+              <img src="@/assets/images/avatar-avicha.png" alt="Avichal Garg" />
+            </div>
+            <div class="infos">
+              <span class="name">Avichal Garg</span>
+              <span class="designation"
+                >Co-Founder and Managing Partner, Electric Capital</span
+              >
+            </div>
+          </a>
+        </li>
+        <li>
+          <a href="#" class="card-instructors">
+            <div class="avatar">
+              <img src="@/assets/images/avatar-alex.png" alt="Alex Pruden" />
+            </div>
+            <div class="infos">
+              <span class="name">Alex Pruden</span>
+              <span class="designation">CEO, Aleo</span>
+            </div>
+          </a>
+        </li>
+        <li>
+          <a href="#" class="card-instructors">
+            <div class="avatar">
+              <img src="@/assets/images/avatar-jacob.png" alt="Jacob Horne" />
+            </div>
+            <div class="infos">
+              <span class="name">Jacob Horne</span>
+              <span class="designation">Co-Founder, Zora</span>
+            </div>
+          </a>
+        </li>
+        <li>
+          <a href="#" class="card-instructors">
+            <div class="avatar">
+              <img src="@/assets/images/avatar-soona.png" alt="Soona Amhaz" />
+            </div>
+            <div class="infos">
+              <span class="name">Soona Amhaz</span>
+              <span class="designation">Managing Partner, Volt Capital</span>
+            </div>
+          </a>
+        </li>
+        <li>
+          <a href="#" class="card-instructors">
+            <div class="avatar">
+              <img
+                src="@/assets/images/avatar-morgan.png"
+                alt="Morgan Beller"
+              />
+            </div>
+            <div class="infos">
+              <span class="name">Morgan Beller</span>
+              <span class="designation">General Partner, NFX</span>
+            </div>
+          </a>
+        </li>
+        <li>
+          <a href="#" class="card-instructors">
+            <div class="avatar">
+              <img src="@/assets/images/avatar-jil.png" alt="Jill Gunter" />
+            </div>
+            <div class="infos">
+              <span class="name">Jill Gunter</span>
+              <span class="designation">Co-Founder, Espresso Systems</span>
+            </div>
+          </a>
+        </li>
+        <li>
+          <a href="#" class="card-instructors">
+            <div class="avatar">
+              <img src="@/assets/images/avatar-dani.png" alt="Dani Grant" />
+            </div>
+            <div class="infos">
+              <span class="name">Dani Grant</span>
+              <span class="designation">Analyst, Union Square Ventures</span>
+            </div>
+          </a>
+        </li>
+        <li>
+          <a href="#" class="card-instructors">
+            <div class="avatar">
+              <img
+                src="@/assets/images/avatar-michael.png"
+                alt="Michael Jordan"
+              />
+            </div>
+            <div class="infos">
+              <span class="name">Michael Jordan</span>
+              <span class="designation">Investor, Galaxy Digital</span>
+            </div>
+          </a>
+        </li>
+        <li>
+          <a href="#" class="card-instructors">
+            <div class="avatar">
+              <img
+                src="@/assets/images/avatar-spencer.png"
+                alt="Spencer Noon"
+              />
+            </div>
+            <div class="infos">
+              <span class="name">Spencer Noon</span>
+              <span class="designation">Co-Founder, Variant Fund</span>
+            </div>
+          </a>
+        </li>
+        <li>
+          <a href="#" class="card-instructors">
+            <div class="avatar">
+              <img src="@/assets/images/avatar-niraj.png" alt="Niraj Pant" />
+            </div>
+            <div class="infos">
+              <span class="name">Niraj Pant</span>
+              <span class="designation"
+                >General Partner, Polychain Capital</span
+              >
+            </div>
+          </a>
+        </li>
+        <li>
+          <a href="#" class="card-instructors">
+            <div class="avatar">
+              <img
+                src="@/assets/images/avatar-haseeb.png"
+                alt="Haseeb Qureshi"
+              />
+            </div>
+            <div class="infos">
+              <span class="name">Haseeb Qureshi</span>
+              <span class="designation"
+                >Managing Partner, Dragonfly Capital</span
+              >
+            </div>
+          </a>
+        </li>
+        <li>
+          <a href="#" class="card-instructors">
+            <div class="avatar">
+              <img src="@/assets/images/avatar-dan-r.png" alt="Dan Robinson" />
+            </div>
+            <div class="infos">
+              <span class="name">Dan Robinson</span>
+              <span class="designation">Research Partner, Paradigm</span>
+            </div>
+          </a>
+        </li>
+        <li>
+          <a href="#" class="card-instructors">
+            <div class="avatar">
+              <img
+                src="@/assets/images/avatar-kartik.png"
+                alt="Kartik Talwar"
+              />
+            </div>
+            <div class="infos">
+              <span class="name">Kartik Talwar</span>
+              <span class="designation"
+                >General Partner, ACapital Ventures</span
+              >
+            </div>
+          </a>
+        </li>
+        <li>
+          <a href="#" class="card-instructors">
+            <div class="avatar">
+              <img src="@/assets/images/avatar-nick.png" alt="Nick Tomaino" />
+            </div>
+            <div class="infos">
+              <span class="name">Nick Tomaino</span>
+              <span class="designation"
+                >Founder and General Partner, 1confirmation</span
+              >
+            </div>
+          </a>
+        </li>
+        <li>
+          <a href="#" class="card-instructors">
+            <div class="avatar">
+              <img src="@/assets/images/avatar-alok.png" alt="Alok Vasudev" />
+            </div>
+            <div class="infos">
+              <span class="name">Alok Vasudev</span>
+              <span class="designation">Co-Founder, Standard Crypto</span>
+            </div>
+          </a>
+        </li>
+        <li>
+          <a href="#" class="card-instructors">
+            <div class="avatar">
+              <img src="@/assets/images/avatar-linda.png" alt="Linda Xie" />
+            </div>
+            <div class="infos">
+              <span class="name">Linda Xie</span>
+              <span class="designation">Co-Founder, Scalar</span>
+            </div>
+          </a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Instructors",
+};
+</script>
