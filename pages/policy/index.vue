9720019f94173337cda918a079d6c3c894a11816
olehyev138 <oleg.ye@yahoo.com>
2022-11-23 23:42:57 +0600

    convert policy page

---
+++ pages/policy/index.vue
@@ -0,0 +1,513 @@
+<template>
+  <div id="wrapper">
+    <header class="hero hero--sm hero--policy">
+      <div class="container">
+        <picture class="bg">
+          <source
+            media="(min-width: 768px)"
+            srcset="@/assets/images/bg-banner-policy.svg"
+          />
+          <source srcset="@/assets/images/bg-banner-policy-sm.svg" />
+          <img src="@/assets/images/bg-banner-policy.svg" alt="" />
+        </picture>
+        <div class="highlight-display hld--lavender">
+          <h1>Policy</h1>
+        </div>
+      </div>
+    </header>
+
+    <section class="text-block">
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-6">
+            <h5>
+              The a16z crypto policy team focuses on working with founders,
+              lawmakers, and regulators around the globe to help shape the
+              legislation that will govern the future of web3.
+            </h5>
+          </div>
+          <div class="col-sm-6">
+            <div class="desc">
+              <p>
+                At a16z crypto, we support clear, thoughtful, and responsible
+                regulation of web3. We believe in crypto's transparent ethos and
+                are dedicated to making policy open and understandable for all.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <div class="section-divider">
+      <div class="container">
+        <span class="block-title">Principles</span>
+      </div>
+    </div>
+
+    <div class="sub-headline">
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-6">
+            <h6>
+              To unlock the full potential of web3, we support the following
+              core principles while working with lawmakers around the globe:
+            </h6>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <section class="principles">
+      <div class="container">
+        <div class="row principle-boxes">
+          <div class="col-sm-6">
+            <div class="inner">
+              <div class="icon-wrap">
+                <img src="@/assets/images/icon01.svg" width="24" alt="" />
+              </div>
+              <h6>
+                Public policy that advances technology for the next generation
+                of the internet
+              </h6>
+              <p>
+                Innovation creates jobs, keeps the U.S. competitive and a world
+                leader and, and secures the future for all. Crypto and web3 are
+                critical to this next phase of the internet and industry, and so
+                we will always take a pro-innovation stance.
+              </p>
+            </div>
+          </div>
+          <div class="col-sm-6">
+            <div class="inner">
+              <div class="icon-wrap">
+                <img src="@/assets/images/icon02.svg" width="24" alt="" />
+              </div>
+              <h6>
+                Non-partisan or bipartisan, and cross-agency, collaboration
+              </h6>
+              <p>
+                Web3 is one of the rare policy conversations that can bridge the
+                political spectrum. This growing industry and technology
+                includes a host of activities and opportunities that demand
+                different expertise in a variety of domains that cut across
+                agencies. To effectively legislate, all perspectives should be
+                considered across various agencies working together for an
+                enduring long-term set of solutions.
+              </p>
+            </div>
+          </div>
+          <div class="col-sm-6">
+            <div class="inner">
+              <div class="icon-wrap">
+                <img src="@/assets/images/icon03.svg" width="24" alt="" />
+              </div>
+              <h6>Responsible regulation of businesses, not software</h6>
+              <p>
+                Fundamentally we believe that all legislation and regulation
+                should focus on businesses and not the protocols or technology
+                itself. This has a long standing precedent, but has become
+                increasingly important with new web3 use cases.
+              </p>
+            </div>
+          </div>
+          <div class="col-sm-6">
+            <div class="inner">
+              <div class="icon-wrap">
+                <img src="@/assets/images/icon04.svg" width="24" alt="" />
+              </div>
+              <h6>Transparent rule making</h6>
+              <p>
+                We believe transparent policymaking is the most effective and
+                long-lasting way to provide clear “rules of the road” for the
+                web3 community and consumers.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <div class="section-divider">
+      <div class="container">
+        <span class="block-title">Team</span>
+      </div>
+    </div>
+
+    <section class="team">
+      <div class="container">
+        <ul class="highlight-list style--1">
+          <li>
+            <a href="#" class="card-people">
+              <div class="avatar decor--2">
+                <img alt="Ben Horowitz" src="@/assets/images/avatar-ben.png" />
+              </div>
+              <div class="r-col">
+                <div class="name">Ben Horowitz</div>
+                <span class="post">Cofounder & General partner</span>
+              </div>
+            </a>
+          </li>
+          <li>
+            <a href="#" class="card-people">
+              <div class="avatar decor--1">
+                <img
+                  alt="Anthony Albanese"
+                  src="https://a16zcrypto.com/wp-content/uploads/2022/05/AnthonyAlbanese-Photo-300x300.jpg"
+                />
+              </div>
+              <div class="r-col">
+                <div class="name">Anthony Albanese</div>
+                <span class="post">Cheif operating officer</span>
+              </div>
+            </a>
+          </li>
+          <li>
+            <a href="#" class="card-people">
+              <div class="avatar decor--3">
+                <img
+                  alt="Miles Jenningse"
+                  src="https://a16zcrypto.com/wp-content/uploads/2022/05/Miles-Jennings-300x300.png"
+                />
+              </div>
+              <div class="r-col">
+                <div class="name">Miles Jenningse</div>
+                <span class="post">crypto general counsel</span>
+              </div>
+            </a>
+          </li>
+          <li>
+            <a href="#" class="card-people">
+              <div class="avatar decor--4">
+                <img
+                  alt="Michele Korver"
+                  src="https://a16zcrypto.com/wp-content/uploads/2022/08/Michele-Korver-300x300.png"
+                />
+              </div>
+              <div class="r-col">
+                <div class="name">Michele Korver</div>
+                <span class="post">Head of regulatory</span>
+              </div>
+            </a>
+          </li>
+          <li>
+            <a href="#" class="card-people">
+              <div class="avatar decor--5">
+                <img
+                  alt="Jai Ramaswamy"
+                  src="@/assets/images/avatar-jai-ramaswamy.png"
+                />
+              </div>
+              <div class="r-col">
+                <div class="name">Jai Ramaswamy</div>
+                <span class="post">Cheif Legal Officer</span>
+              </div>
+            </a>
+          </li>
+          <li>
+            <a href="#" class="card-people">
+              <div class="avatar decor--6">
+                <img
+                  alt="Scott Walker"
+                  src="@/assets/images/avatar-scott.png"
+                />
+              </div>
+              <div class="r-col">
+                <div class="name">Scott Walker</div>
+                <span class="post">Chief Compliance Officer</span>
+              </div>
+            </a>
+          </li>
+          <li>
+            <a href="#" class="card-people">
+              <div class="avatar decor--7">
+                <img
+                  alt="Collin McCune"
+                  src="@/assets/images/avatar-collin.png"
+                />
+              </div>
+              <div class="r-col">
+                <div class="name">Collin McCune</div>
+                <span class="post">head of government affairs</span>
+              </div>
+            </a>
+          </li>
+          <li>
+            <a href="#" class="card-people">
+              <div class="avatar decor--7">
+                <img alt="Bill Hinman" src="@/assets/images/avatar-bill.png" />
+              </div>
+              <div class="r-col">
+                <div class="name">Bill Hinman</div>
+                <span class="post">advisory partner </span>
+              </div>
+            </a>
+          </li>
+          <li>
+            <a href="#" class="card-people">
+              <div class="avatar decor--1">
+                <img
+                  alt="Bill Hinman"
+                  src="@/assets/images/avatar-brian-q.png"
+                />
+              </div>
+              <div class="r-col">
+                <div class="name">Brian Quintenz</div>
+                <span class="post">advisory partner </span>
+              </div>
+            </a>
+          </li>
+          <li>
+            <a href="#" class="card-people">
+              <div class="avatar decor--2">
+                <img
+                  alt="Caroline Friedman"
+                  src="https://a16zcrypto.com/wp-content/uploads/2022/11/Approved-10-240x300.jpg"
+                />
+              </div>
+              <div class="r-col">
+                <div class="name">Caroline Friedman</div>
+                <span class="post">operations</span>
+              </div>
+            </a>
+          </li>
+          <li>
+            <a href="#" class="card-people">
+              <div class="avatar decor--1">
+                <img
+                  alt="Caroline Friedman"
+                  src="https://a16zcrypto.com/wp-content/uploads/2022/06/David-Sverdlov-300x300.png"
+                />
+              </div>
+              <div class="r-col">
+                <div class="name">David Sverdlov</div>
+                <span class="post">regulatory counsel</span>
+              </div>
+            </a>
+          </li>
+          <li>
+            <a href="#" class="card-people">
+              <div class="avatar decor--3">
+                <img
+                  alt="Caroline Friedman"
+                  src="https://a16zcrypto.com/wp-content/uploads/2022/06/Paul-Cafiero-300x300.png"
+                />
+              </div>
+              <div class="r-col">
+                <div class="name">Paul Cafiero</div>
+                <span class="post">Communications partner</span>
+              </div>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </section>
+
+    <div class="section-divider">
+      <div class="container">
+        <span class="block-title">Resources</span>
+        <a href="#" class="btn-link underlined">
+          see all
+          <span class="icon-arrow-right"></span>
+        </a>
+      </div>
+    </div>
+
+    <section class="categories">
+      <div class="container">
+        <div class="category-row">
+          <div class="group-header bg-grey">
+            <h6>for everyone</h6>
+          </div>
+          <div class="article">
+            <div class="row">
+              <div class="col-sm-2">
+                <span class="category-title">09.02.22 / article</span>
+              </div>
+              <div class="col-sm-10 col-md-8">
+                <h6><a href="#">why web3 matters</a></h6>
+                <span class="posted-by"> <a href="#">Chris Dixon</a></span>
+              </div>
+            </div>
+          </div>
+          <div class="article">
+            <div class="row">
+              <div class="col-sm-2">
+                <span class="category-title">09.29.22 / report</span>
+              </div>
+              <div class="col-sm-10 col-md-8">
+                <h6>
+                  <a href="#">introducing the 2022 state of crypto report</a>
+                </h6>
+                <span class="posted-by">
+                  <a href="#">Daren Matsuoka</a>, <a href="#">Eddy Lazzarin</a>,
+                  <a href="#">Chris Dixon</a> and
+                  <a href="#">Robert Hackett</a></span
+                >
+              </div>
+            </div>
+          </div>
+          <div class="article">
+            <div class="row">
+              <div class="col-sm-2">
+                <span class="category-title">09.29.22 / paper</span>
+              </div>
+              <div class="col-sm-10 col-md-8">
+                <h6><a href="#">the web3 landscape</a></h6>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="category-row">
+          <div class="group-header bg-grey">
+            <h6>for lawmakers</h6>
+          </div>
+          <div class="article">
+            <div class="row">
+              <div class="col-sm-2">
+                <span class="category-title">12.04.22 / article </span>
+              </div>
+              <div class="col-sm-10 col-md-8">
+                <h6><a href="#">legal frameworks for DAOs series</a></h6>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="category-row">
+          <div class="group-header bg-grey">
+            <h6>for builders</h6>
+          </div>
+          <div class="article">
+            <div class="row">
+              <div class="col-sm-2">
+                <span class="category-title">11.12.21 / article</span>
+              </div>
+              <div class="col-sm-10 col-md-8">
+                <h6><a href="#">the can’t be evil NFT licenses</a></h6>
+                <span class="posted-by">
+                  <a href="#">Miles Jennings</a> and
+                  <a href="#">Chris Dixon</a></span
+                >
+              </div>
+            </div>
+          </div>
+          <div class="article">
+            <div class="row">
+              <div class="col-sm-2">
+                <span class="category-title">08.08.22 / article</span>
+              </div>
+              <div class="col-sm-10 col-md-8">
+                <h6>
+                  <a href="#"
+                    >decentralization for web3 builders: principles, models,
+                    how</a
+                  >
+                </h6>
+                <span class="posted-by"> <a href="#">Miles Jennings</a></span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <div class="section-divider bg-grey">
+      <div class="container">
+        <span class="block-title">Positions</span>
+      </div>
+    </div>
+
+    <section class="profile-featured bg-grey">
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-4">
+            <div class="box">
+              <span class="category-title">op-eds /</span>
+              <h6><a href="#">in defense of stablecoins</a></h6>
+              <div class="authors">
+                <a href="#">Miles Jennings</a>
+              </div>
+            </div>
+          </div>
+          <div class="col-sm-4">
+            <div class="box">
+              <span class="category-title">comment letter /</span>
+              <h6>
+                <a href="#"
+                  >“as a part of a regular business” in the definition of dealer
+                  and government securities dealer</a
+                >
+              </h6>
+              <div class="authors">
+                <a href="#">Ms. Vanessa A. Countryman</a>
+              </div>
+            </div>
+          </div>
+          <div class="col-sm-4">
+            <div class="box">
+              <span class="category-title">paper /</span>
+              <h6><a href="#">proposed-amicus-brief</a></h6>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <div class="section-divider">
+      <div class="container">
+        <span class="block-title">announcements</span>
+        <a href="#" class="btn-link underlined">
+          see all
+          <span class="icon-arrow-right"></span>
+        </a>
+      </div>
+    </div>
+
+    <section class="featured">
+      <div class="container">
+        <div class="article">
+          <div class="row">
+            <div class="col-sm-2">
+              <span class="category-title">10.20.22 / article</span>
+            </div>
+            <div class="col-sm-10 col-md-8">
+              <h6><a href="#">Collin McCune</a></h6>
+              <span class="posted-by"> <a href="#">Anthony Albanese</a></span>
+            </div>
+          </div>
+        </div>
+        <div class="article">
+          <div class="row">
+            <div class="col-sm-2">
+              <span class="category-title">10.20.22 / article</span>
+            </div>
+            <div class="col-sm-10 col-md-8">
+              <h6>
+                <a href="#"
+                  >On Crypto and Its Implications for American Technology
+                  Innovation</a
+                >
+              </h6>
+              <span class="posted-by"> <a href="#">Scott Kupor</a></span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <Footer></Footer>
+  </div>
+</template>
+
+<script>
+export default {
+  head() {
+    return {
+      title: "Policy",
+    };
+  },
+  name: "Policy",
+};
+</script>
