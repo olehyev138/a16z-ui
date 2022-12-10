34edb5064d110a3ff73b21396988c7a2ce832c58
olehyev138 <oleg.ye@yahoo.com>
2022-12-10 22:01:38 +0600

    implement API on portfolio

--- api/portfoliopage.js
+++ api/portfoliopage.js
@@ -1,35 +1,29 @@
 export default (axios) => ({
-  get() {
-    // return axios
-    //   .get("wp/v2/pages/7")
-    //   .then((response) => {
-    //     return response.data;
-    //   })
-    //   .catch((error) => {
-    //     console.log(error);
-    //   });
+  getCompanies(payload) {
     return new Promise(function (resolve, reject) {
+      const config = {
+        params: payload,
+      };
       axios
-        .get("wp/v2/pages/7")
-        .then(function (response) {
-          resolve(response.data.acf);
-        })
-        .catch(function (error) {
-          console.log("Show error notification! ", error);
-          reject(error);
-        });
-    });
-  },
-
-  getPopularTags() {
-    return new Promise(function (resolve, reject) {
-      axios
-        .get("/api/v1/fetch-tags")
+        .get("/api/v1/fetch-posts", config)
         .then(function (response) {
           resolve(response.data);
         })
         .catch(function (error) {
-          console.log("Show error notification! ", error);
+          // console.log("Show error notification! ", error);
+          reject(error);
+        });
+    });
+  },
+  getJobs() {
+    return new Promise(function (resolve, reject) {
+      axios
+        .get("/wp/v2/pages/94")
+        .then(function (response) {
+          resolve(response.data.acf);
+        })
+        .catch(function (error) {
+          // console.log("Show error notification! ", error);
           reject(error);
         });
     });
